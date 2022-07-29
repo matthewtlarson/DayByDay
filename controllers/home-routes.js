20db220cf81724952ce1f6fb13268a53dcfa3511
Matthew Larson <106900634+matthewtlarson@users.noreply.github.com>
2022-07-29 14:33:18 -0700

    Update home-routes.js
    homeroutes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,31 +1,16 @@
-//takes from the model to the controller to the view//
-
-
-
-
-/*const router = require('express').Router();
-const { User } = require('../models');
+const router = require('express').Router();
+const { Profile } = require('../models');
+const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
-  // TODO: Render template with Sequelize data
-  try {
-    const dbUserData = await User.findAll ({
-      order: [
-        ["name", "ASC"]
-      ],
-      attributes: {
-        exclude: ["password"]
-      }
-    });
-
-
-    const users = userData.map(user => user.get({plain: true}))
-
-
-  res.render('homepage', { users });
-  } catch (err) {
-    res.status(500).json(err);
-  }
+  // Send the rendered Handlebars.js template back as the response
+  res.render('welcome'); //awaiting the user information to take the user to main
+});
+router.get('/signup', async (req, res) => {
+  res.render('signup');
+});
+router.get('/main', async (req, res) => { //grab all of the user info (graph data progress favorites etc)
+  res.render('main');
 });
 
-module.exports = router;*/
+module.exports = router;
